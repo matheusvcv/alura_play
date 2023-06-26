f0bc234837e8ba85227e3530d0eb5a23a82b5d9b
Matheus Vinicius <matheusvcv@yahoo.com.br>
2023-06-26 17:54:07 -0300

    Criada a pasta public para que o servidor web não tenha acesso aos arquivos que possuem logicas e regras e negocio em php, e possa acessar somente os arquivos necessários.

--- inicio-html.php
+++ inicio-html.php
@@ -17,7 +17,7 @@
         <nav class="cabecalho">
             <a class="logo" href="./"></a>
             <div class="cabecalho__icones">
-                <a href="./pages/enviar-video.html" class="cabecalho__videos"></a>
+                <a href="/novo-video" class="cabecalho__videos"></a>
                 <a href="./pages/login.html" class="cabecalho__sair">Sair</a>
             </div>
         </nav>
